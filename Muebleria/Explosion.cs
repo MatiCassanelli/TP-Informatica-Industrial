a0e5fdb1cbc23d94d02b3a014c8d4c2c07a913c2
Matias Cassanelli <matiascassanelli@hotmail.com>
2017-08-26 17:42:32 -0300

    Explosion recursiva lista

--- Muebleria/Explosion.cs
+++ Muebleria/Explosion.cs
@@ -12,6 +12,8 @@ namespace Muebleria
 {
     public partial class Explosion : Form
     {
+
+        informatica_industrial_dbEntities db = new informatica_industrial_dbEntities();
         public Explosion()
         {
             InitializeComponent();
@@ -19,9 +21,7 @@ namespace Muebleria
         }
 
         private void LlenarCombos()
-        {
-            informatica_industrial_dbEntities db = new informatica_industrial_dbEntities();
-            
+        {           
             //Consulta para traer la lista de los nombres de los productos de "intermedio buy", "intermedio make" y "bruto"
             var subquery = from p in db.producto
                            where p.idTipo == 1 || p.idTipo == 3
@@ -35,27 +35,30 @@ namespace Muebleria
 
             cbProductos.DataSource = query.ToList();
         }
+        private string CortarCadena(string c)
+        {
+            String[] substrings = c.Split(new string[] { "  x" }, StringSplitOptions.None);
+            return substrings[0];
+        }
 
         private void ActualizarListBox()
         {
             lbComponentes.Items.Clear();
-
-            lbComponentes.DataSource = ConsultarRecursivo(cbProductos.SelectedItem.ToString());
+            ConsultarRecursivo(cbProductos.SelectedItem.ToString(), cbProductos.SelectedItem.ToString());
         }
-
-        private List<string> ConsultarRecursivo(string padre)
+                
+        private List<PadreHijo> consultarPadre(string padre)
         {
-            informatica_industrial_dbEntities db = new informatica_industrial_dbEntities();
-            int cantRequerida = Convert.ToInt32(tbCantidad.Text);
+            int CantRequerida = Convert.ToInt32(tbCantidad.Text);
             //Obtener los id de los productos hijos del padre seleccionado en el cbProducto
             var query = from ph in db.padre_componente
                         from t in db.traduccion
                         from p in db.producto
                         where p.idProducto == ph.idPadre &&
                         p.idDescriptionP == t.idDescriptionT &&
-                        t.Traduccion_str == cbProductos.SelectedItem.ToString() &&
+                        t.Traduccion_str == padre &&
                         t.idLanguageT == LogIn.IdIdioma
-                        select new { id_Hijo = ph.idHijo, cant = ph.Cantidad * cantRequerida };
+                        select new { id_Hijo = ph.idHijo, cant = ph.Cantidad * CantRequerida };
 
             //Obtener las descripciones de las unidades de medida de la cantidad de aplicacion
             var subquery = from um in db.unidad_medida
@@ -63,7 +66,7 @@ namespace Muebleria
                            where um.idDescriptionUM == t.idDescriptionT &&
                            t.idLanguageT == LogIn.IdIdioma
                            select new { id = um.idUnidad_Medida, t = t.Traduccion_str };
-
+            
             //Obtener las descripciones de los productos seleccionados en la consulta anterior
             var query2 = from n in query
                          from sq in subquery
@@ -73,17 +76,35 @@ namespace Muebleria
                          p.idDescriptionP == t.idDescriptionT &&
                          sq.id == p.Unidad_id_Aplication &&
                          t.idLanguageT == LogIn.IdIdioma
-                         select t.Traduccion_str + "  x" + n.cant.ToString() + " " + sq.t;
-            //select new { Componente = t.Traduccion_str, Cantidad = n.cant };
-            List<string> resultadoquery = query2.ToList();
-            List<string> hijos = new List<string>();
-
-            foreach (string item in resultadoquery)
+                         //select t.Traduccion_str + "  x" + n.cant.ToString() + " " + sq.t;
+                         select new {nombre = t.Traduccion_str, cant = n.cant.ToString(), um = sq.t };
+            List<PadreHijo> lista = new List<PadreHijo>();
+            
+            foreach (var item in query2)
             {
-                hijos.Add(item);
-                hijos.AddRange(ConsultarRecursivo(item));
+                lista.Add(new PadreHijo(padre, item.nombre, Convert.ToInt32(item.cant), item.um));
             }
-            return hijos;
+            return lista;
+        }
+
+        List<PadreHijo> hijos = new List<PadreHijo>();
+
+        private void ConsultarRecursivo(string padre, string prodfinal)
+        {            
+            List<PadreHijo> aux = consultarPadre(padre);
+            if (aux.Count > 0)
+            {
+                foreach (PadreHijo item in aux)
+                {
+                    string tabs = "";
+                    if (prodfinal != item.Padre)
+                        tabs += '\t';
+
+                    lbComponentes.Items.Add(tabs+item.Hijo + " x " + item.Cantidad + ' ' + item.Um);
+                    ConsultarRecursivo(item.Hijo,prodfinal);
+                }
+            }
+
         }
 
         private void Explosion_FormClosed(object sender, FormClosedEventArgs e)
