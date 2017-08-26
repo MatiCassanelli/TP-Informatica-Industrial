362f8bd3aed68cf68bdeecdde0e810c0dadb69a1
Felipe <felipetoledo4815@gmail.com>
2017-08-26 17:48:40 -0300

    ABM en proceso

--- Muebleria/Composicion.designer.cs
+++ Muebleria/Composicion.designer.cs
@@ -29,7 +29,10 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.btn_Eliminar = new System.Windows.Forms.Button();
             this.lbCargadas = new System.Windows.Forms.ListBox();
@@ -42,13 +45,16 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.cbUM = new System.Windows.Forms.ComboBox();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.cbProductos = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
-            this.label6 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblProdSeleccionado = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.panel1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.panelComponentes.SuspendLayout();
             this.panelCantidades.SuspendLayout();
@@ -57,40 +63,73 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.groupBox3);
+            this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.groupBox2);
             this.panel1.Controls.Add(this.panelComponentes);
+            this.panel1.Controls.Add(this.monthCalendar1);
             this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.label5);
             this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(840, 452);
+            this.panel1.Size = new System.Drawing.Size(1007, 692);
             this.panel1.TabIndex = 0;
             // 
-            // label5
+            // groupBox3
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(439, 16);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(176, 13);
-            this.label5.TabIndex = 0;
-            this.label5.Text = "Seleccione la fecha de publicación:";
+            this.groupBox3.Controls.Add(this.comboBox1);
+            this.groupBox3.Controls.Add(this.label9);
+            this.groupBox3.Controls.Add(this.lblProdSeleccionado);
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Controls.Add(this.listBox1);
+            this.groupBox3.Location = new System.Drawing.Point(433, 205);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(409, 260);
+            this.groupBox3.TabIndex = 23;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Sustitutos";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(291, 231);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Eliminar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(6, 80);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(397, 130);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(782, 633);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 23);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "Publicar";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.button1);
-            this.groupBox2.Controls.Add(this.label6);
-            this.groupBox2.Controls.Add(this.monthCalendar1);
             this.groupBox2.Controls.Add(this.btn_Eliminar);
-            this.groupBox2.Controls.Add(this.label5);
             this.groupBox2.Controls.Add(this.lbCargadas);
-            this.groupBox2.Location = new System.Drawing.Point(8, 214);
+            this.groupBox2.Location = new System.Drawing.Point(8, 205);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(821, 221);
+            this.groupBox2.Size = new System.Drawing.Size(409, 260);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Componentes";
             // 
             // btn_Eliminar
             // 
-            this.btn_Eliminar.Location = new System.Drawing.Point(278, 177);
+            this.btn_Eliminar.Location = new System.Drawing.Point(291, 231);
             this.btn_Eliminar.Name = "btn_Eliminar";
             this.btn_Eliminar.Size = new System.Drawing.Size(112, 23);
             this.btn_Eliminar.TabIndex = 4;
@@ -100,12 +139,11 @@
             // 
             // lbCargadas
             // 
-            this.lbCargadas.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lbCargadas.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.lbCargadas.FormattingEnabled = true;
-            this.lbCargadas.Location = new System.Drawing.Point(6, 38);
+            this.lbCargadas.Location = new System.Drawing.Point(6, 41);
             this.lbCargadas.Name = "lbCargadas";
-            this.lbCargadas.Size = new System.Drawing.Size(384, 130);
+            this.lbCargadas.Size = new System.Drawing.Size(397, 169);
             this.lbCargadas.TabIndex = 0;
             // 
             // panelComponentes
@@ -116,7 +154,7 @@
             this.panelComponentes.Enabled = false;
             this.panelComponentes.Location = new System.Drawing.Point(8, 73);
             this.panelComponentes.Name = "panelComponentes";
-            this.panelComponentes.Size = new System.Drawing.Size(821, 134);
+            this.panelComponentes.Size = new System.Drawing.Size(409, 126);
             this.panelComponentes.TabIndex = 1;
             // 
             // label4
@@ -146,9 +184,9 @@
             this.panelCantidades.Controls.Add(this.label1);
             this.panelCantidades.Controls.Add(this.cbUM);
             this.panelCantidades.Enabled = false;
-            this.panelCantidades.Location = new System.Drawing.Point(9, 41);
+            this.panelCantidades.Location = new System.Drawing.Point(5, 41);
             this.panelCantidades.Name = "panelCantidades";
-            this.panelCantidades.Size = new System.Drawing.Size(381, 81);
+            this.panelCantidades.Size = new System.Drawing.Size(395, 81);
             this.panelCantidades.TabIndex = 21;
             // 
             // tbCantidad
@@ -161,7 +199,7 @@
             // 
             // btnCargar
             // 
-            this.btnCargar.Location = new System.Drawing.Point(285, 55);
+            this.btnCargar.Location = new System.Drawing.Point(292, 55);
             this.btnCargar.Name = "btnCargar";
             this.btnCargar.Size = new System.Drawing.Size(93, 23);
             this.btnCargar.TabIndex = 20;
@@ -187,7 +225,7 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(5, 17);
+            this.label1.Location = new System.Drawing.Point(4, 16);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(49, 13);
             this.label1.TabIndex = 21;
@@ -197,10 +235,16 @@
             // 
             this.cbUM.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbUM.FormattingEnabled = true;
-            this.cbUM.Location = new System.Drawing.Point(254, 13);
+            this.cbUM.Location = new System.Drawing.Point(251, 13);
             this.cbUM.Name = "cbUM";
-            this.cbUM.Size = new System.Drawing.Size(124, 21);
+            this.cbUM.Size = new System.Drawing.Size(134, 21);
             this.cbUM.TabIndex = 20;
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(521, 494);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 1;
             // 
             // groupBox1
             // 
@@ -232,42 +276,56 @@
             this.label3.TabIndex = 22;
             this.label3.Text = "Productos";
             // 
-            // monthCalendar1
+            // label5
             // 
-            this.monthCalendar1.Location = new System.Drawing.Point(442, 38);
-            this.monthCalendar1.Name = "monthCalendar1";
-            this.monthCalendar1.TabIndex = 1;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(518, 472);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(176, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Seleccione la fecha de publicación:";
             // 
-            // label6
+            // lblProdSeleccionado
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(3, 16);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(72, 13);
-            this.label6.TabIndex = 5;
-            this.label6.Text = "Componentes";
+            this.lblProdSeleccionado.AutoSize = true;
+            this.lblProdSeleccionado.Location = new System.Drawing.Point(172, 16);
+            this.lblProdSeleccionado.Name = "lblProdSeleccionado";
+            this.lblProdSeleccionado.Size = new System.Drawing.Size(35, 13);
+            this.lblProdSeleccionado.TabIndex = 6;
+            this.lblProdSeleccionado.Text = "label8";
+            this.lblProdSeleccionado.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
-            // button1
+            // label9
             // 
-            this.button1.Location = new System.Drawing.Point(703, 177);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 23);
-            this.button1.TabIndex = 22;
-            this.button1.Text = "Publicar";
-            this.button1.UseVisualStyleBackColor = true;
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(6, 41);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(165, 13);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "Seleccione un producto sustituto:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(177, 38);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(226, 21);
+            this.comboBox1.TabIndex = 24;
             // 
             // Composicion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1001, 684);
+            this.ClientSize = new System.Drawing.Size(1067, 716);
             this.Controls.Add(this.panel1);
             this.Name = "Composicion";
             this.Text = "Composición de los Productos";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Composicion_FormClosed);
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
             this.panelComponentes.ResumeLayout(false);
             this.panelComponentes.PerformLayout();
             this.panelCantidades.ResumeLayout(false);
@@ -298,8 +356,13 @@
         private System.Windows.Forms.Button btn_Eliminar;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label6;
         private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lblProdSeleccionado;
     }
 }
 
