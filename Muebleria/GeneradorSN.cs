31eb29a3d0b782f12855807a8a838c4fdf6768c0
Mati <matiascassanelli@gmail.com>
2017-09-06 02:20:52 -0300

    Calcular digito validador

--- Muebleria/GeneradorSN.cs
+++ Muebleria/GeneradorSN.cs
@@ -33,11 +33,31 @@ namespace Muebleria
         public void digitoValidador(int sn)
         {
             string par = sn.ToString();
-            string aux= "";
-            for (int i = 1; i < par.Count(); i+=2)
+            List<int> aux = new List<int>();
+            for (int i = 0; i < par.Count(); i++)
             {
-                aux+= par[i];
+                aux.Add(int.Parse(par[i].ToString()));
             }
+
+            int x = 0, y = 0;
+            for (int i = 1; i < aux.Count; i+=2)
+            {
+                x += aux[i];
+            }
+            for (int i = 0; i < aux.Count; i+=2)
+            {
+                if (y > 9)
+                    y += (2 * aux[i] - 9);
+                else
+                    y += (2 * aux[i]);
+            }
+            int z = x + y;
+
+            int t = z % 10;
+
+            int d = 10 - t;
+            
+
         }
 
     }
