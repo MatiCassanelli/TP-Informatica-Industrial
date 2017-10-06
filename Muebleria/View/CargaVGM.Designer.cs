8ac3ca3434a64ac76c48277bb4e0946e9d57762c
Mati <matiascassanelli@gmail.com>
2017-10-06 12:51:00 -0300

    Interfaz carga de pedidos

---
+++ Muebleria/View/CargaVGM.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Muebleria.View
+{
+    partial class CargaVGM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelCantidades = new System.Windows.Forms.Panel();
+            this.tbCantidad = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbUM = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbCliente = new System.Windows.Forms.ComboBox();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.panel1.SuspendLayout();
+            this.panelCantidades.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.groupBox2);
+            this.panel1.Controls.Add(this.panelCantidades);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(921, 747);
+            this.panel1.TabIndex = 0;
+            // 
+            // panelCantidades
+            // 
+            this.panelCantidades.Controls.Add(this.tbCantidad);
+            this.panelCantidades.Controls.Add(this.label3);
+            this.panelCantidades.Controls.Add(this.label4);
+            this.panelCantidades.Controls.Add(this.cbUM);
+            this.panelCantidades.Location = new System.Drawing.Point(5, 132);
+            this.panelCantidades.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.panelCantidades.Name = "panelCantidades";
+            this.panelCantidades.Size = new System.Drawing.Size(878, 110);
+            this.panelCantidades.TabIndex = 26;
+            // 
+            // tbCantidad
+            // 
+            this.tbCantidad.Location = new System.Drawing.Point(666, 40);
+            this.tbCantidad.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.tbCantidad.Name = "tbCantidad";
+            this.tbCantidad.Size = new System.Drawing.Size(182, 38);
+            this.tbCantidad.TabIndex = 23;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 43);
+            this.label3.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 32);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Producto";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(520, 43);
+            this.label4.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(130, 32);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Cantidad";
+            // 
+            // cbUM
+            // 
+            this.cbUM.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbUM.FormattingEnabled = true;
+            this.cbUM.Location = new System.Drawing.Point(150, 40);
+            this.cbUM.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.cbUM.Name = "cbUM";
+            this.cbUM.Size = new System.Drawing.Size(325, 39);
+            this.cbUM.TabIndex = 20;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cbCliente);
+            this.groupBox1.Location = new System.Drawing.Point(8, 7);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.groupBox1.Size = new System.Drawing.Size(518, 123);
+            this.groupBox1.TabIndex = 25;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Cliente";
+            // 
+            // cbCliente
+            // 
+            this.cbCliente.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbCliente.FormattingEnabled = true;
+            this.cbCliente.Location = new System.Drawing.Point(174, 49);
+            this.cbCliente.Name = "cbCliente";
+            this.cbCliente.Size = new System.Drawing.Size(298, 39);
+            this.cbCliente.TabIndex = 0;
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(25, 40);
+            this.monthCalendar1.Margin = new System.Windows.Forms.Padding(22);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 27;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.monthCalendar1);
+            this.groupBox2.Location = new System.Drawing.Point(8, 276);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(687, 438);
+            this.groupBox2.TabIndex = 28;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Semana para que lo necesita";
+            // 
+            // CargaVGM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(955, 771);
+            this.Controls.Add(this.panel1);
+            this.Name = "CargaVGM";
+            this.Text = "CargaVGM";
+            this.panel1.ResumeLayout(false);
+            this.panelCantidades.ResumeLayout(false);
+            this.panelCantidades.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panelCantidades;
+        private System.Windows.Forms.TextBox tbCantidad;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbUM;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbCliente;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+    }
+}
--- Muebleria/View/MRP.Designer.cs
+++ Muebleria/View/MRP.Designer.cs
@@ -31,10 +31,10 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.btnAceptar = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnNuevo = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -61,10 +61,19 @@
             // panel1
             // 
             this.panel1.Controls.Add(this.dataGridView1);
-            this.panel1.Location = new System.Drawing.Point(12, 79);
+            this.panel1.Location = new System.Drawing.Point(12, 75);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(900, 639);
             this.panel1.TabIndex = 26;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(20, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 40;
+            this.dataGridView1.Size = new System.Drawing.Size(877, 632);
+            this.dataGridView1.TabIndex = 0;
             // 
             // btnAceptar
             // 
@@ -84,30 +93,22 @@
             this.button1.Text = "Eliminar";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnNuevo
             // 
-            this.button2.Location = new System.Drawing.Point(964, 587);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(139, 55);
-            this.button2.TabIndex = 29;
-            this.button2.Text = "Nuevo";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 40;
-            this.dataGridView1.Size = new System.Drawing.Size(894, 632);
-            this.dataGridView1.TabIndex = 0;
+            this.btnNuevo.Location = new System.Drawing.Point(964, 587);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(139, 55);
+            this.btnNuevo.TabIndex = 29;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
             // 
             // MRP
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1285, 730);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(1302, 751);
+            this.Controls.Add(this.btnNuevo);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnAceptar);
             this.Controls.Add(this.panel1);
@@ -115,6 +116,7 @@
             this.Controls.Add(this.label1);
             this.Name = "MRP";
             this.Text = "MRP";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MRP_FormClosed);
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
@@ -130,6 +132,6 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button btnAceptar;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnNuevo;
     }
 }
