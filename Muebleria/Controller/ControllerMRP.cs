dd8243e57ffca94e15ca37cee8fae1aae913ef98
Mati <matiascassanelli@gmail.com>
2017-10-07 17:58:05 -0300

    Carga una unica vez los requerimientos con datos iguales, sumando los deltas

--- Muebleria/Controller/ControllerMRP.cs
+++ Muebleria/Controller/ControllerMRP.cs
@@ -13,12 +13,16 @@ namespace Muebleria
             List<requerimientos> aux = lista;
             for (int i = 0; i < lista.Count; i++)
             {
-                for (int j = 1; j < aux.Count && i!=j; j++)
+                for (int j = 1; j < aux.Count && i != j; j++)
                 {
                     if (lista[i].idProducto == aux[j].idProducto)
                         if (lista[i].Cliente == aux[j].Cliente)
                             if (lista[i].Semana == aux[j].Semana)
+                            {
                                 lista[i].Delta += aux[j].Delta;
+                                lista.Remove(lista[j]);
+                                j = i;
+                            }
                 }
             }
             return lista;
